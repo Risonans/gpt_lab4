1c151741c26c3fcf3153be848a7bf0000eb7b7c4
Nick271618 <nick.costareff@yandex.ru>
2024-10-17 12:19:06 +0500

    fct

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,16 +1,16 @@
 import blogData from "../mockData/blogData";
 
-export const blog_card_1 = ({ blog_card_1_Data }) => {
+/* export const blog_card_1 = ({ blog_card_1_Data }) => {
     const { src, alt, data, title_1, button: { href, articles } } = blog_card_1_Data;
     return (
         <>
-        <div class="blog_articles_first">
-            <img className="blog_articles_img_first" src={src} alt={alt} />
-            <div class="blog_text_1">
-                <div class="blog_articles_data">{data}</div>
-                <h4 class="blog_articles_title_1">{title_1}</h4>
-                <nav class="blog_articles_nav_1">
-                    <ul class="blog_articles_link_1">
+        <div className="blog_articles_first">
+            <img src={src} alt={alt} />
+            <div className="blog_text_1">
+                <div className="blog_articles_data">{data}</div>
+                <h4 className="blog_articles_title_1">{title_1}</h4>
+                <nav className="blog_articles_nav_1">
+                    <ul className="blog_articles_link_1">
                         <li><a href={href}>{articles}</a></li>
                     </ul>
                 </nav>
@@ -18,7 +18,17 @@ export const blog_card_1 = ({ blog_card_1_Data }) => {
         </div>
         </>
     );
-};
+}; */
+
+export const blog_card_1 = ({ blog_card_1_Data }) => {
+    const { data, title_1 } = blog_card_1_Data;
+    return (
+      <>
+        <div className="blog_articles_data">{data}</div>
+        <h4 className="blog_articles_title_1">{title_1}</h4>
+      </>
+    );
+  };
 
 const Blog = () => {
     const {blog_card_1_Data} = blogData;
