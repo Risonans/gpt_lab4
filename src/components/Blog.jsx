0d1f128c14e2d5dba06fcd7f0811ed843e915c21
Nick271618 <nick.costareff@yandex.ru>
2024-10-17 13:47:38 +0500

    13.47
    Nick (готово)

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,6 @@
 import blogData from "../mockData/blogData";
 
-/* export const blog_card_1 = ({ blog_card_1_Data }) => {
+export const BlogCard1 = ({ blog_card_1_Data }) => {
     const { src, alt, data, title_1, button: { href, articles } } = blog_card_1_Data;
     return (
         <>
@@ -18,25 +18,44 @@ import blogData from "../mockData/blogData";
         </div>
         </>
     );
-}; */
+};
 
-export const blog_card_1 = ({ blog_card_1_Data }) => {
-    const { data, title_1 } = blog_card_1_Data;
+export const BlogCard2 = ({ blog_card_2_Data }) => {
+    const { src, alt, data, title_1, button: { href, articles } } = blog_card_2_Data;
     return (
-      <>
-        <div className="blog_articles_data">{data}</div>
-        <h4 className="blog_articles_title_1">{title_1}</h4>
-      </>
+        <>
+        <div className="blog_articles">
+            <img src={src} alt={alt} />
+            <div className="blog_text">
+                <div className="blog_articles_data">{data}</div>
+                <h4 className="blog_articles_title">{title_1}</h4>
+                <nav className="blog_articles_nav">
+                    <ul className="blog_articles_link">
+                        <li><a href={href}>{articles}</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </>
     );
-  };
+};
+
 
 const Blog = () => {
-    const {blog_card_1_Data} = blogData;
+    const {blog_card_1_Data, blog_card_2_Data, blog_card_3_Data, blog_card_4_Data, blog_card_5_Data} = blogData;
 
     return (
         <>
         <div className="con_1">
-            <blog_card_1 blog_card_1_Data={blog_card_1_Data}/>
+            <BlogCard1 blog_card_1_Data={blog_card_1_Data} />
+        </div>
+        <div className="con_2">
+            <BlogCard2 blog_card_2_Data={blog_card_2_Data} />
+            <BlogCard2 blog_card_2_Data={blog_card_3_Data} />
+        </div>
+        <div className="con_3">
+            <BlogCard2 blog_card_2_Data={blog_card_4_Data} />
+            <BlogCard2 blog_card_2_Data={blog_card_5_Data} />
         </div>
         </>
     );
